8c55ff7884a27f108cd3e7fb257a891541a76e3c
Ubuntu <gfsilveira@gmail.com>
2022-08-04 17:24:50 +0000

    Inserindo análise de tempo de execussão

--- script/start-py/main.py
+++ script/start-py/main.py
@@ -1,3 +1,5 @@
+from time import process_time
+
 from src.inicio import Inicio
 
 
@@ -12,5 +14,28 @@ def teste_inicio() -> str:
     return "Sistema não está funcionando"
 
 
+def teste_de_velocidade(n: int) -> None:
+    # Start the stopwatch / counter
+    t1_start = process_time()
+
+    soma = 0
+    for i in range(n):
+        soma += 1
+
+    print(soma)
+
+    # Stop the stopwatch / counter
+    t1_stop = process_time()
+
+    print("Elapsed time:", t1_stop, "s", t1_start, "s")
+
+    print(
+        "Elapsed time during the whole program in seconds:",
+        t1_stop-t1_start, "s"
+    )
+
+
 if __name__ == "__main__":
     print(teste_inicio())
+
+    teste_de_velocidade(n=100_000_000)
